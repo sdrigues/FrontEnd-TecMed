3f84bc9ddd1b60abcdcb003af8adb5e1be1acf35
sdrigues <rodrigues.sam97@gmail.com>
2020-03-02 11:01:04 -0300

    finalizado a tela do qrcode

--- src/pages/QrCode_Guias/index.js
+++ src/pages/QrCode_Guias/index.js
@@ -1,25 +1,45 @@
 import React from 'react';
 import {
     Container,
+    Image,
     ViewTop,
+    Title,
+    TitleGuia,
+    TextQrcode,
     ViewBody,
-    
+    ViewQrCode,
+    BtnVoltar,
+    TextBtnVoltar
+
 } from './styles';
 import QRCode from 'react-native-qrcode-svg';
+
+import Logo from  '../../assets/images/Logo.jpg';
 
 export default function QrcodeGuias({navigation, route}){
     const { ID } = route.params;
     return(
         <Container>
+            <Image source={Logo} />
             <ViewTop>
-
+                <Title>Guia</Title>
+                    <TitleGuia>
+                        ID: 1234567
+                        {/* {JSON.stringify(ID._id)} */}
+                    </TitleGuia>
             </ViewTop>
             <ViewBody>
-                <QRCode
-                    value={JSON.stringify(ID._id)}
-                />
+                <TextQrcode>Apresente esse QRCode ao Médico para validar sua consulta!!!</TextQrcode>
+                <ViewQrCode>
+                    <QRCode
+                        value={JSON.stringify(ID._id)}
+                        size={180}
+                    />
+                </ViewQrCode>
+                <BtnVoltar onPress={() => navigation.pop()} >
+                    <TextBtnVoltar>Voltar</TextBtnVoltar>
+                </BtnVoltar>
             </ViewBody>
-            
         </Container>
     );
 }
--- src/pages/QrCode_Guias/styles.js
+++ src/pages/QrCode_Guias/styles.js
@@ -1,13 +1,59 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  margin-left: 30px;
-  margin-top: 30px;
+  flex: 1;
+  background-color: #FFF;
+`;
+
+export const Image = styled.Image`
+    width: 110px;
+    height: 110px;
+    margin-left: 65%;
 `;
 export const ViewTop = styled.View`
+    /* flex-direction: row; */
+`;
 
+export const Title = styled.Text`
+    font-size: 35px;
+    color: #5EE0B6;
+    margin-top: 3%;
+    margin-left: 40%;
+`;
+export const TitleGuia = styled.Text`
+    font-size: 20px;
+    color: rgba(0,0,0,0.5);
+    margin-top: 5%;
+    margin-left: 35%;
+`;
+
+export const TextQrcode = styled.Text`
+    color: rgba(0,0,0,0.5);
+    margin-left: 8%;
+    font-size: 17px;
+    margin-top: 5%;
+    margin-bottom: 5%;
+`;
+export const ViewQrCode = styled.View`
+    margin-top: 5%;
+    margin-left: 25%;
+    width: 30%;
+    height: 30%;
 `;
 
 export const ViewBody = styled.View`
 
+`;
+
+export const BtnVoltar = styled.TouchableOpacity`
+    width: 30%;
+    height: 35px;
+    margin-top: 30%;
+    margin-left: 43%;
+`;
+
+export const TextBtnVoltar = styled.Text`
+    color: #5EE0B6;
+    margin-top: 15px;
+    font-size: 18px;
 `;
